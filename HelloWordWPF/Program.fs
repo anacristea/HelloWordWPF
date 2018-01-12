f5c3403b724b756f42a24143ced1c78c992deb48
Cristea Anamaria <acristea@pitechnologies.ro>
2018-01-12 14:29:55 +0200

    First commit

---
+++ HelloWordWPF/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+
+let loadWindow()=
+    let resourceLocator = new Uri("/HelloWordWPF;component/MainWindow.xaml", UriKind.Relative)
+    let window = Application.LoadComponent(resourceLocator) :?> Window 
+    (window.FindName("clickButton"):?> Button).Click.Add(
+        fun _ -> MessageBox.Show("Hello World") |> ignore)
+    window
+
+[<STAThread>]
+(new Application()).Run(loadWindow()) |> ignore
+
